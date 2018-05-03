8ad021111e1559bf45f4736242e9c99fc309f9ba
Simon Tippe <tippex97@gmail.com>
2018-05-03 14:52:25 +0200

    module_update updated

--- module_update.bat
+++ module_update.bat
@@ -1,1 +1,10 @@
-git submodule foreach git pull origin master
+git submodule deinit -f -- "Monogame Engine/Engine"
+RD /S /Q ".git/modules/Engine"
+git rm -f "Monogame Engine/Engine"
+git submodule add --name "Engine" https://github.com/SilverbackSoPra/Engine.git "Monogame Engine/Engine"
+git submodule init
+git submodule deinit -f -- "Monogame Engine/Content/Shader"
+RD /S /Q ".git/modules/Shader"
+git rm -f "Monogame Engine/Content/Shader"
+git submodule add --name "Shader" https://github.com/SilverbackSoPra/Shader.git "Monogame Engine/Content/Shader"
+git submodule init
